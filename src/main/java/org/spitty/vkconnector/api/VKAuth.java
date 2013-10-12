5d9e37257bc46144122fe033f0e8cb28db8054ab
spitty <spitty.pnz@gmail.com>
2013-10-12 21:57:54 +0400

    Fix bug with not set authSuccessful variable in authenticate() method.

--- src/main/java/org/spitty/vkconnector/api/VKAuth.java
+++ src/main/java/org/spitty/vkconnector/api/VKAuth.java
@@ -1,8 +1,5 @@
 package org.spitty.vkconnector.api;
 
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -11,8 +8,11 @@ import org.jsoup.select.Elements;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
- *
  * @author spitty
  */
 public class VKAuth {
@@ -25,7 +25,7 @@ public class VKAuth {
     private String redirectURL = DEFAULT_REDIRECT_URL;
     private String accessToken;
     private String userID;
-    private boolean authSuccessfull = false;
+    private boolean authSuccessful = false;
     private boolean authProcessed = false;
 
     public VKAuth(String clientID, String email, String password) {
@@ -33,14 +33,23 @@ public class VKAuth {
         this.email = email;
         this.password = password;
         authProcessed = false;
-        authSuccessfull = false;
+        authSuccessful = false;
         accessToken = null;
     }
 
+    /**
+     * This constructor takes {@code access_token} and {@code user_id} which can be specified without using plain text
+     * credentials.<br/>
+     * You can obtain accessToken by yourself with this link:
+     * http://oauth.vk.com/oauth/authorize?client_id=3913198&redirect_uri=http://oauth.vk.com/blank.html&scope=wall,friends&display=page&response_type=token
+     *
+     * @param accessToken access_token
+     * @param userID      user_id
+     */
     public VKAuth(String accessToken, String userID) {
         this.accessToken = accessToken;
         this.userID = userID;
-        authSuccessfull = true;
+        authSuccessful = true;
         authProcessed = true;
     }
 
@@ -49,17 +58,9 @@ public class VKAuth {
     }
 
     /**
-     * You can obtain accessToken by yourself with this link:
-     * http://oauth.vk.com/oauth/authorize?client_id=3913198&redirect_uri=http://oauth.vk.com/blank.html&scope=wall,friends&display=page&response_type=token
-     */
-    public void setAccessToken(String accessToken) {
-        this.accessToken = accessToken;
-    }
-
-    /**
      * Used in requests to VK API methods
      *
-     * @return
+     * @return {@link String} access_token
      */
     public String getAccessToken() {
         checkAuthSuccess();
@@ -71,12 +72,8 @@ public class VKAuth {
         return userID;
     }
 
-    public void setClientID(String clientID) {
-        this.clientID = clientID;
-    }
-
-    public boolean isAuthSuccessfull() {
-        return authSuccessfull;
+    public boolean isAuthSuccessful() {
+        return authSuccessful;
     }
 
     private String getEmail() {
@@ -87,7 +84,7 @@ public class VKAuth {
         return password;
     }
 
-    public void authentificate() {
+    public void authenticate() {
         try {
             Map<String, String> params = new HashMap<>();
             if (getClientID() == null || getPassword() == null || getEmail() == null) {
@@ -129,14 +126,14 @@ public class VKAuth {
                     .data(paramsToSend)
                     .execute();
             String url = execute.url().toString();
-            // Check if we are authentificated
+            // Check if we are authenticated
             if (!url.contains("access_token")) {
                 LOGGER.warn("\"access_token\" absents. Probably credentials are wrong");
                 LOGGER.trace("Response body: {}", execute.body());
                 Document errorLoginPage = execute.parse();
                 String warnMessage = errorLoginPage.select("div.service_msg_warning").text();
                 authProcessed = true;
-                authSuccessfull = false;
+                authSuccessful = false;
                 throw new IllegalArgumentException("Authentification fails. Please check credentials. VK warn message is "
                         + "\"" + warnMessage + "\"");
             }
@@ -144,7 +141,7 @@ public class VKAuth {
             String urlParams = url.substring(url.indexOf("#") + 1);
             LOGGER.debug("URL params: {}", urlParams);
             Map<String, String> result = new HashMap<>();
-            for (String keyValue : urlParams.split("\\&")) {
+            for (String keyValue : urlParams.split("&")) {
                 String s[] = keyValue.split("=");
                 if (s.length == 2) {
                     result.put(s[0], s[1]);
@@ -152,24 +149,25 @@ public class VKAuth {
             }
             LOGGER.debug("Parsed params: {}", result);
 
-            // Get usefull parameters: access_token and user_id
+            // Get useful parameters: access_token and user_id
             accessToken = result.get("access_token");
             userID = result.get("user_id");
             LOGGER.debug("Access token: {}", accessToken);
             LOGGER.debug("User ID: {}", userID);
+            authSuccessful = true;
         } catch (IOException ex) {
-            LOGGER.error("Can't authentificate", ex);
-            authSuccessfull = false;
+            LOGGER.error("Can't authenticate", ex);
+            authSuccessful = false;
         }
         authProcessed = true;
     }
 
     private void checkAuthSuccess() throws IllegalStateException {
         if (!authProcessed) {
-            authentificate();
+            authenticate();
         }
-        if (!authSuccessfull) {
-            throw new IllegalStateException("Can't authentificate with specified credentials");
+        if (!authSuccessful) {
+            throw new IllegalStateException("Can't authenticate with specified credentials");
         }
     }
 }
