176d3c2ad01f35001b0e2b08da1876647cb078c8
Maxim Logunov <spitty.pnz@gmail.com>
2014-12-14 00:22:52 +0300

    Don't use "Gson" in API naming

--- src/main/java/org/spitty/vkconnector/util/JsonUtils.java
+++ src/main/java/org/spitty/vkconnector/util/JsonUtils.java
@@ -1,20 +1,21 @@
-package org.spitty.vkconnector.gson;
+package org.spitty.vkconnector.util;
 
 import com.google.gson.Gson;
+import org.spitty.vkconnector.model.Response;
 
 /**
  * Utility class containing static methods to converting {@link String} JSON in
  * Java {@link Object Objects}
  */
-public class GsonHelper {
+public class JsonUtils {
 
     private final static Gson GSON = new Gson();
 
     /**
-     * Convert input JSON into {@link Response}
+     * Convert input JSON into {@link org.spitty.vkconnector.model.Response}
      *
      * @param stringResponse JSON {@link String}
-     * @return {@link Response} corresponding input JSON
+     * @return {@link org.spitty.vkconnector.model.Response} corresponding input JSON
      */
     public static Response convertJsonToResponse(String stringResponse) {
         return GSON.fromJson(stringResponse, Response.class);
